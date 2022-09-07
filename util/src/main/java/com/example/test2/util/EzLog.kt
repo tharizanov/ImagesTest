ea628dd4a8df756b4f9942bb77fc32e12a57cdce
tharizanov <tsvetelin.harizanov@gmail.com>
2022-09-07 08:36:11 +0300

    Improve logger

--- util/src/main/java/com/example/test2/util/EzLog.kt
+++ util/src/main/java/com/example/test2/util/EzLog.kt
@@ -3,20 +3,21 @@ package com.example.test2.util
 import android.util.Log
 
 /**
- * My personal custom logger class, which only posts logs to [Log]
- * if the current build variant is [BuildConfig.DEBUG].
+ * Custom logger class, which only posts logs to [Log] if the current build variant is [BuildConfig.DEBUG].
  */
 @Suppress("unused")
 object EzLog {
 
-    private const val TAG_PREFIX = "-----"
-    private const val MESSAGE_SEPARATOR = ", "
-    private const val EMPTY_MESSAGE = "-"
+    private const val EMPTY_MESSAGE = "_"
+    private const val DEFAULT_MESSAGE_SEPARATOR = ", "
+
+    private val TAG = javaClass.simpleName
 
     /**
      * If TRUE, the log will contain the whole stacktrace leading to this call, otherwise just the message.
      */
-	var includeFullStackTrace = false
+    @JvmStatic
+    var includeFullStackTrace = false
 
     /**
      * VERBOSE level log.
@@ -26,7 +27,7 @@ object EzLog {
     @JvmStatic
     fun v(vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.v(buildTag(), buildMessage(MESSAGE_SEPARATOR, objects, includeFullStackTrace))
+            Log.v(TAG, buildMessage(objects))
     }
 
     /**
@@ -38,7 +39,7 @@ object EzLog {
     @JvmStatic
     fun v_(separator: String, vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.v(buildTag(), buildMessage(separator, objects, includeFullStackTrace))
+            Log.v(TAG, buildMessage(objects, separator))
     }
 
     /**
@@ -49,7 +50,7 @@ object EzLog {
     @JvmStatic
     fun d(vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.d(buildTag(), buildMessage(MESSAGE_SEPARATOR, objects, includeFullStackTrace))
+            Log.d(TAG, buildMessage(objects))
     }
 
     /**
@@ -61,7 +62,7 @@ object EzLog {
     @JvmStatic
     fun d_(separator: String, vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.d(buildTag(), buildMessage(separator, objects, includeFullStackTrace))
+            Log.d(TAG, buildMessage(objects, separator))
     }
 
     /**
@@ -72,7 +73,7 @@ object EzLog {
     @JvmStatic
     fun i(vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.i(buildTag(), buildMessage(MESSAGE_SEPARATOR, objects, includeFullStackTrace))
+            Log.i(TAG, buildMessage(objects))
     }
 
     /**
@@ -84,7 +85,7 @@ object EzLog {
     @JvmStatic
     fun i_(separator: String, vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.i(buildTag(), buildMessage(separator, objects, includeFullStackTrace))
+            Log.i(TAG, buildMessage(objects, separator))
     }
 
     /**
@@ -95,7 +96,7 @@ object EzLog {
     @JvmStatic
     fun w(vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.w(buildTag(), buildMessage(MESSAGE_SEPARATOR, objects, includeFullStackTrace))
+            Log.w(TAG, buildMessage(objects))
     }
 
     /**
@@ -107,7 +108,7 @@ object EzLog {
     @JvmStatic
     fun w_(separator: String, vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.w(buildTag(), buildMessage(separator, objects, includeFullStackTrace))
+            Log.w(TAG, buildMessage(objects, separator))
     }
 
     /**
@@ -118,7 +119,7 @@ object EzLog {
     @JvmStatic
     fun e(vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.e(buildTag(), buildMessage(MESSAGE_SEPARATOR, objects, includeFullStackTrace))
+            Log.e(TAG, buildMessage(objects))
     }
 
     /**
@@ -130,7 +131,7 @@ object EzLog {
     @JvmStatic
     fun e_(separator: String, vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.e(buildTag(), buildMessage(separator, objects, includeFullStackTrace))
+            Log.e(TAG, buildMessage(objects, separator))
     }
 
     /**
@@ -141,7 +142,7 @@ object EzLog {
     @JvmStatic
     fun wtf(vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.wtf(buildTag(), buildMessage(MESSAGE_SEPARATOR, objects, includeFullStackTrace))
+            Log.wtf(TAG, buildMessage(objects))
     }
 
     /**
@@ -153,45 +154,69 @@ object EzLog {
     @JvmStatic
     fun wtf_(separator: String, vararg objects: Any?) {
         if (BuildConfig.DEBUG)
-            Log.wtf(buildTag(), buildMessage(separator, objects, includeFullStackTrace))
+            Log.wtf(TAG, buildMessage(objects, separator))
     }
 
-    /**
-     * @return A human readable string representation of the [StackTraceElement].
-     */
-    private fun stackTraceToString(ste: StackTraceElement): String =
-        "${ste.className.substringAfterLast('.')}.${ste.methodName}()"
+
+
+    private var callingFunctionStackIndex = -1
 
     /**
-     * @return A log tag that includes the source of the log call.
-     */
-    private fun buildTag(): String =
-        "$TAG_PREFIX${stackTraceToString(Thread.currentThread().stackTrace[4])}"
-
-    /**
-     * @param separator The separator to split the message parts.
      * @param messageParts Objects to be fused into a readable log message.
+     * @param separator The separator to split the message parts.
+     * @param elements Array of [StackTraceElement]s.
      * @param isFullStackTrace See [includeFullStackTrace].
      * @return The message to display in the log.
      */
-    private fun buildMessage(separator: String, messageParts: Array<out Any?>, isFullStackTrace: Boolean): String =
-        if (isFullStackTrace) {
-            val messageBuilder = StringBuilder(buildMessage(separator, messageParts, false))
-            Thread.currentThread().stackTrace.run {
-                if (size > 5)
-                    for (i in 5 until size)
-                        messageBuilder.append("\n").append(stackTraceToString(this[i]))
-            }
-            messageBuilder.toString()
-        } else {
+    private fun buildMessage(
+        messageParts: Array<out Any?>,
+        separator: String = DEFAULT_MESSAGE_SEPARATOR,
+        elements: Array<StackTraceElement> = Thread.currentThread().stackTrace,
+        isFullStackTrace: Boolean = includeFullStackTrace
+    ): String {
+        findCallingFunctionStackIndex(elements)
+
+        return StringBuilder().apply {
+            // Append all message parts
             when (messageParts.size) {
-                0 -> EMPTY_MESSAGE
-                1 -> messageParts[0].toString()
-                else -> StringBuilder("${messageParts[0]}").apply {
+                0 -> append(EMPTY_MESSAGE)
+                1 -> append(nonEmptyMsg(messageParts[0]))
+                else -> {
+                    append(nonEmptyMsg(messageParts[0]))
                     for (i in 1 until messageParts.size)
-                        append(separator).append(messageParts[i])
-                }.toString()
+                        append(separator).append(nonEmptyMsg(messageParts[i]))
+                }
+            }
+
+            // Now append the stack trace
+            when {
+                callingFunctionStackIndex < 0 -> {}
+                isFullStackTrace -> {
+                    for (i in callingFunctionStackIndex until elements.size)
+                        append("\n").append(steNameForMessage(elements[i]))
+                }
+                else -> append("\n").append(steNameForMessage(elements[callingFunctionStackIndex]))
+            }
+        }.toString()
+    }
+
+    private fun nonEmptyMsg(obj: Any?): String =
+        obj.toString().ifEmpty { EMPTY_MESSAGE }
+
+    private fun steNameForMessage(ste: StackTraceElement): String =
+        "at ${ste.className}.${ste.methodName}()"
+
+    private fun findCallingFunctionStackIndex(elements: Array<StackTraceElement>) {
+        if (callingFunctionStackIndex >= 0)
+            return
+
+        val thisClassName = javaClass.name
+        elements.forEachIndexed { index, stackTraceElement ->
+            if (thisClassName == stackTraceElement.className) {
+                callingFunctionStackIndex = index + 1
+                return@forEachIndexed
             }
         }
+    }
 
 }
